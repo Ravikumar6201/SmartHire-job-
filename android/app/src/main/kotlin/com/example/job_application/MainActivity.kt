da3d55bca34903eb3375cf217d6d6f57369dd8ae
Ravikumar251099 <ravi.logicnexus@gmail.com>
2025-01-04 12:57:57 +0530

    Apply updated .gitignore rules

--- android/app/src/main/kotlin/com/example/job_application/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.logicnexus.smarthire
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/Common/Url.dart
+++
@@ -1,5 +0,0 @@
-// ignore_for_file: file_names
-
-class ApiConstants {
-  static const String baseUrl = 'http://smarthire.shandilyam.com/beta/admin/';
-}
